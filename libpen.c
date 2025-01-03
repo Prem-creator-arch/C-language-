3f1af744c40223f91bbb0e5996d86a31d3ed2d11
Prem Kumar <premmasterofspeed@gmail.com>
2025-01-03 19:45:56 +0530

    Add files via upload

---
+++ libpen.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int  main()
+{
+	int n,m,pages,price;
+	float r;
+	char name[20],book[40];
+	printf("\t**WELCOME**\n TO THE E-LIBRARY OF THE INSTITUTE!!\n\n");
+	printf("What is your name?\n");
+	scanf("%19s",name);
+	printf("What is the name of the book?\n");
+	scanf("%39s",book);
+	printf("How many pages does the book has?\n");
+	scanf("%d",&pages);
+	printf("what is the price of the book?\n");
+	scanf("%d",&price);
+	printf("Do you want to:\n 1.Borrow\n 2.Pay due fees\n");
+	scanf("%d",&m);
+	switch(m)
+	{
+		case 1:
+			printf("Report For Borrowing:");
+			printf("Name of the borrower:%c",&name);
+			printf("Name of the book:%c",&book);
+			printf("No. of pages of the book:%d",pages);
+			printf("Price of the book:%d",price);
+			printf("You can issue the book!!");
+		break;
+		case 2:
+			printf("Report For Borrowing:");
+			printf("Name of the borrower:%c",&name);
+			printf("Name of the book:%c",&book);
+			printf("No. of pages of the book:%d",pages);
+			printf("Price of the book:%d",price);
+			printf("How many days did you have the book?\n");
+			scanf("%d",&n);
+			if (n<5)
+			r=n*0.5;
+			printf("The rent is %.2f",r);
+			if (n>=5)
+			r=(n-5)*0.5;
+			printf("The rent is %.2f",r);
+			break;
+		}
+	return 0;
+}
